cba4d55a604c2b7b096cacda539b4eb56620a45a
DonalCrotty <donalcrotty15@gmail.com>
2018-08-18 16:33:26 +0100

    Input validation, Implemented React-Router, seperated out pages

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Header = props => {
-  const {branding} = props;
+  const { branding } = props;
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">{branding}</a>
+        <a href="/" className="navbar-brand">
+          {branding}
+        </a>
         <div>
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
-              <a href="/" className="nav-link">Home</a>
+              <a href="/" className="nav-link">
+                Home
+              </a>
             </li>
             <li className="nav-item">
-              <a href="/" className="nav-link">About</a>
+              <a href="/about" className="nav-link">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="/contact/add" className="nav-link">
+                Add Contact
+              </a>
             </li>
           </ul>
         </div>
@@ -23,9 +35,9 @@ const Header = props => {
 };
 Header.defaultProps = {
   branding: 'My App'
-}
+};
 Header.propTypes = {
   branding: PropTypes.string.isRequired
-}
+};
 
 export default Header;
